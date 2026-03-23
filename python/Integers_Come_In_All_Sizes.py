81ecec15482fe00aebe22f7f7cc8619a0350c3a5
achal_deshmukh <achaldeshmukh112@gmail.com>
2026-03-23 17:23:02 +0530

    Create Integers_Come_In_All_Sizes.py solution

---
+++ python/Integers_Come_In_All_Sizes.py
@@ -0,0 +1,15 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+res1 = pow(a, b)
+res2 = pow(c, d)
+print(res1 + res2)
+
+# Input (stdin)
+# 9
+# 29
+# 7
+# 27
+# Output
+# 4710194409608608369201743232
