77959a32473c2ebd3424f6813eed0861116f70c8
achal_deshmukh <achaldeshmukh112@gmail.com>
2026-04-14 13:46:06 +0530

    Create athlete_sort.py solution file

---
+++ python/athlete_sort.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    k = int(input().strip())
+    
+    sorted_table = sorted(arr, key = lambda row : row[k])
+    for row in sorted_table:
+        print(*row)
+
+# Input (stdin)
+# 5 3
+# 10 2 5
+# 7 1 0
+# 9 9 9
+# 1 23 12
+# 6 5 9
+# 1
+# Output
+# 7 1 0
+# 10 2 5
+# 6 5 9
+# 9 9 9
+# 1 23 12
